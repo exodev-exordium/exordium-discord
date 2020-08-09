498743a0ed0e29f57115989046c479e2a71a6103
FearGannicus <gannicus@exordium.org>
2020-08-09 00:23:20 -0400

    update; reformat file and fix requirements

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// dotenv requirement for token data
+const dotenv = require('dotenv');
+dotenv.config();
+
+// discord requirement
 const Discord = require('discord.js');
-const { Client, Collection, Util } = require("discord.js");
-var { token, sigcolor, prefix } = require('./config.json');
+
 const client = new Discord.Client();
 const bot = new Discord.Client({ disableEveryone: true });
+
 bot.commands = new Discord.Collection();
-
-
-client.commands = new Collection();
-client.aliases = new Collection();
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 ["command"].forEach(handler => {
 	require(`./hand/${handler}`)(client);
@@ -18,15 +21,15 @@ client.once('ready', () => {
 	client.user.setActivity(`with Exordium | +help`, {
 		type: `PLAYING`,
 	});
-})
+});
 
 client.on('message', async message => {
 	if (message.author.bot) return;
 	if (!message.guild) return;
-	if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(process.env.DISCORD_PREFIX)) return;
 
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const args = message.content.slice(process.env.DISCORD_PREFIX.length).trim().split(/ +/g);
 	const cmd = args.shift().toLowerCase();
 
 	if (cmd.length === 0) return;
@@ -37,4 +40,4 @@ client.on('message', async message => {
 	if (command)
 		command.run(client, message, args);
 });
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
